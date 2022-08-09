597532b81c78fd5edc75d837fd23350f8bc6ab41
ThiagoMachadoL <thiagojkp123@gmail.com>
2022-08-08 21:52:45 -0300

    exercício de fixação 13

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,36 @@
-let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+// let nacionalidade = prompt("Escreva aqui sua nacionalidade").toLowerCase().replace(/o$/,"a")
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
-}
+// if(nacionalidade === "brasileira"){
+//     console.log("A pessoa é brasileira");
+// }else if(nacionalidade === "argentina"){
+// 	console.log("A pessoa é argentina ");
+// }else if(nacionalidade === "uruguaia"){
+//     console.log("A pessoa é uruguaia");
+// }else if(nacionalidade === "chilena"){
+//     console.log("A pessoa é chilena");
+// }else if(nacionalidade === "colombiana"){
+// 	console.log("A pessoa é colombiana");
+// }else{
+//     console.log("Nacionalidade não encontrada");
+// }
+let nacionalidade = prompt("Escreva aqui sua nacionalidade").toLowerCase().replace(/o$/,"a")
+
+switch(nacionalidade){
+    case "brasileira": 
+        console.log("A pessoa é brasileira");
+;        break;
+    case "argentina": 
+        console.log("A pessoa é argentina ");;
+        break;
+    case "uruguaia": 
+        console.log("A pessoa é uruguaia");
+        break;
+    case "chilena": 
+        console.log("A pessoa é chilena");
+        break;
+    case "colombiana":
+        console.log("A pessoa é colombiana");
+        break;
+    default: 
+        console.log("Nacionalidade não identificada");
+} 
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,21 @@
 //código a ser reescrito
+const nome=prompt("Qual o seu nome?".toLowerCase());
+const idade=Number(prompt("Qual a sua idade?"));
+console.log(nome === "jose"? "Oi, Zé" : "Olá "+ nome);
+console.log(idade >=18? "pode tirar carteira de motorista!" : "Ainda não pode tirar carteira de motorista");
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
+
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+
+
